58f6242fefd5adc6d525b1d37ee63794329dfc19
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-21 16:40:33 -0500

    added route for login post request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/posts');
 });
+
+router.post('/login', userController.login);
 
 module.exports = router;
