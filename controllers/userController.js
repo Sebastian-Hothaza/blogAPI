5ba4ef1dd8b2c23d64c92a2f9dac316dc5e4b43a
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-21 17:25:44 -0500

    typo correction

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,8 @@ const User = require('../models/User');
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
-// Home page - all posts
+// Log in page
+// Create and send back token
 exports.login = (req, res, next) => {
     res.send(`TODO: Implement login POST request: ${req.body.password}`);
 };
