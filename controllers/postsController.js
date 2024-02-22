747cbb24cf776adce7c62d6fcdaf4a576dfb2e2a
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-22 14:04:21 -0500

    Added support fot PUT requests on given post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -11,12 +11,23 @@ function validateForm(req,res,next){
     next();
 }
 
-// Home page - retrieves all posts
+// All posts
 exports.index_get = asyncHandler(async (req, res, next) => {
     const posts = await BlogPost.find().exec();   
     return res.json(posts);
 });
 
+// All comments for a specific post
+exports.comments_get = asyncHandler(async (req, res, next) => {
+    const comments = await Comment.find({parentPost: req.params.postID}).exec(); 
+    return res.json(comments);
+});
+
+// Specific post
+exports.post_get = asyncHandler(async(req, res, next) => {
+    const post = await BlogPost.findById(req.params.postID);
+    return res.json(post)
+})
 exports.index_post = [
     body("title", "Title must contain at 6-20 characters").trim().isLength({ min: 6, max: 20}).escape(),
     body("content", "Content must contain at 10-200 characters").trim().isLength({ min: 6, max: 200}).escape(),
@@ -38,25 +49,32 @@ exports.index_post = [
         }))
     },
 ]
+exports.post_put = [
+    body("title", "Title must contain at 6-20 characters").trim().isLength({ min: 6, max: 20}).escape(),
+    body("content", "Content must contain at 10-200 characters").trim().isLength({ min: 6, max: 200}).escape(),
+    validateForm,
+
+    // Verify the token and process the request
+    (req, res, next) => {
+        jwt.verify(req.token, process.env.SECRET_CODE, asyncHandler(async (err, authData) => {
+            if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
+            // User is authenticated; we can edit the post
+            const post = new BlogPost({
+                author: authData.user._id,
+                title: req.body.title,
+                content: req.body.content,
+                timestamp: new Date(),
+                _id: req.params.postID
+            })
+            await BlogPost.findByIdAndUpdate(req.params.postID, post, {});
+            return res.sendStatus(201);
+        }))
+    },
 
 
-    
+]
 
 
-// All comments for a specific post
-exports.comments_get = asyncHandler(async (req, res, next) => {
-    const comments = await Comment.find({parentPost: req.params.postID}).exec(); 
-    res.json(comments);
-});
-
-// Specific post
-exports.post_get = asyncHandler(async(req, res, next) => {
-    const post = await BlogPost.findById(req.params.postID);
-    res.json(post)
-})
-exports.post_put = (req, res, next) => {
-    res.send(`TODO: Implement PUT for specific post: ${req.params.postID}`);
-}
 exports.post_delete = (req, res, next) => {
     res.send(`TODO: Implement DELETE for specific post: ${req.params.postID}`);
 }
