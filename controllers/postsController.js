b5bfe68583a11ebaf6d876721caa6aea2f98597d
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-22 15:09:26 -0500

    Added support for DELETE post requests on specific post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,13 +3,27 @@ const Comment = require('../models/Comment');
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const jwt = require('jsonwebtoken')
+const ObjectId = require('mongoose').Types.ObjectId;
+
 
 // Called by middleware functions
+// Validates the form contents and builds errors array. In case of errors, returns 400 with errors array
 function validateForm(req,res,next){
     const errors = validationResult(req); // Extract the validation errors from a request.
     if (!errors.isEmpty())  return res.status(400).json(errors.mapped());
     next();
 }
+
+// Called by middleware functions
+// Verify that the req.params.postID is a valid objectID and that it exists in our DB
+async function validatePostId(req, res, next){
+    if (!ObjectId.isValid(req.params.postID)) return res.status(404).send({msg: 'postID is not a valid ObjectID'});
+    const postExists = await BlogPost.exists({_id: req.params.postID});
+    console.log(postExists);
+    if (!postExists) return res.status(404).send({msg: 'Post does not exist'});
+    next();
+}
+
 
 // All posts
 exports.index_get = asyncHandler(async (req, res, next) => {
@@ -25,8 +39,12 @@ exports.comments_get = asyncHandler(async (req, res, next) => {
 
 // Specific post
 exports.post_get = asyncHandler(async(req, res, next) => {
-    const post = await BlogPost.findById(req.params.postID);
-    return res.json(post)
+    if (ObjectId.isValid(req.params.postID)){
+        const post = await BlogPost.findById(req.params.postID);
+        return res.json(post)
+    }else{
+        return res.sendStatus(404);
+    } 
 })
 exports.index_post = [
     body("title", "Title must contain at 6-20 characters").trim().isLength({ min: 6, max: 20}).escape(),
@@ -36,7 +54,7 @@ exports.index_post = [
     // Verify the token and process the request
     (req, res, next) => {
         jwt.verify(req.token, process.env.SECRET_CODE, asyncHandler(async (err, authData) => {
-            if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
+            if (err) return res.status(401).send({msg: 'JWT Validation Fail'});
             // User is authenticated; we can create the post
             const post = new BlogPost({
                 author: authData.user._id,
@@ -53,6 +71,7 @@ exports.post_put = [
     body("title", "Title must contain at 6-20 characters").trim().isLength({ min: 6, max: 20}).escape(),
     body("content", "Content must contain at 10-200 characters").trim().isLength({ min: 6, max: 200}).escape(),
     validateForm,
+    validatePostId,
 
     // Verify the token and process the request
     (req, res, next) => {
@@ -70,14 +89,20 @@ exports.post_put = [
             return res.sendStatus(201);
         }))
     },
-
-
 ]
-
-
-exports.post_delete = (req, res, next) => {
-    res.send(`TODO: Implement DELETE for specific post: ${req.params.postID}`);
-}
+exports.post_delete = [
+    validatePostId,
+    (req,res,next)=>{
+        jwt.verify(req.token, process.env.SECRET_CODE, asyncHandler(async (err, authData) => {
+            if (err) return res.status(401).send({msg: 'JWT Validation Fail'});;
+            // User is authenticated; we can delete the post
+    
+            await BlogPost.findByIdAndDelete(req.params.postID);
+            return res.sendStatus(200);
+        }))
+    }
+]
+    
 
 
 // Specific comment
