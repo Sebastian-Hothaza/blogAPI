9cd3110dbbf07366efc95c484358da361b1f8ff7
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-22 13:35:23 -0500

    refactoring

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,29 +4,31 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+// Called by middleware functions
+function validateForm(_,_,next){
+    const errors = validationResult(req); // Extract the validation errors from a request.
+    if (!errors.isEmpty())  return res.status(400).json(errors.mapped());
+    next();
+}
 
 exports.login = [
     body("name", "Title must not be empty").isLength({ min: 1}).escape(),
     body("password", "Password must not be empty").isLength({ min: 1}).escape(),
-
+    validateForm,
+    
+    // Form data is valid. Check that user exists in DB and that password matches
     asyncHandler(async (req, res, next) => {
-        const errors = validationResult(req); // Extract the validation errors from a request.
-        if (!errors.isEmpty())  return res.status(400).json(errors.mapped());
-            
-        // Form data is valid. Check that uses exists in DB and that password matches
         const user = await User.findOne({name: req.body.name}).exec();
         if (user){
-            if (await bcrypt.compare(req.body.password, user.password)){
-                jwt.sign({user}, process.env.SECRET_CODE, (err, token) => res.json({token})); // Create and send back token
+            // Verify Password
+            const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+            if (passwordMatch){
+                jwt.sign({user}, process.env.SECRET_CODE, (err, token) => res.json({token}))
             }else{
-                res.sendStatus(401);
-            }
-            
+                return res.sendStatus(401)
+            }  
         }else{
-            res.status(400).send({msg: 'User not in DB'}); // User does not exist in the db
+            return res.status(400).send({msg: 'User not in DB'}); // User does not exist in the db
         }
     }),
 ]
-
-
-
