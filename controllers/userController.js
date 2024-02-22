e601648bcb2562b10ff0894326dbc076559ddb5e
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-22 13:39:02 -0500

    typo corrections

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 // Called by middleware functions
-function validateForm(_,_,next){
+function validateForm(req,res,next){
     const errors = validationResult(req); // Extract the validation errors from a request.
     if (!errors.isEmpty())  return res.status(400).json(errors.mapped());
     next();
@@ -15,9 +15,9 @@ exports.login = [
     body("name", "Title must not be empty").isLength({ min: 1}).escape(),
     body("password", "Password must not be empty").isLength({ min: 1}).escape(),
     validateForm,
-    
+
     // Form data is valid. Check that user exists in DB and that password matches
-    asyncHandler(async (req, res, next) => {
+    asyncHandler(async (req, res, _) => {
         const user = await User.findOne({name: req.body.name}).exec();
         if (user){
             // Verify Password
