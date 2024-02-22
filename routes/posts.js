cccced8012c0db268ee23352ac5c1dc33da6e91d
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-22 14:03:54 -0500

    Attached tokens to routes needing authorization

--- routes/posts.js
+++ routes/posts.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController')
 
-// Home page - all posts 
+// All posts
 router.get('/', postsController.index_get);
-router.post('/', attachToken, postsController.index_post);
 
 // All comments for a specific post
 router.get('/:postID/comments', postsController.comments_get);
 
 // Specific post
 router.get('/:postID', postsController.post_get);
-router.put('/:postID', postsController.post_put);
-router.delete('/:postID', postsController.post_delete);
+router.post('/', attachToken, postsController.index_post);
+router.put('/:postID', attachToken, postsController.post_put);
+router.delete('/:postID', attachToken, postsController.post_delete);
 
 // Specific comment
 router.get('/:postID/comments/:commentID', postsController.comment_get);
 router.post('/:postID/comments/', postsController.comment_post);
-router.put('/:postID/comments/:commentID', postsController.comment_put);
-router.delete('/:postID/comments/:commentID', postsController.comment_delete);
+router.put('/:postID/comments/:commentID', attachToken, postsController.comment_put);
+router.delete('/:postID/comments/:commentID', attachToken, postsController.comment_delete);
 
 // Attaches token from request header to the request itself
 function attachToken(req, res, next){
